a98a5dbbee6e8100e5c92a8b98a2193ef25ffd15
Ez <ejbennett1@outlook.com>
2025-05-08 16:23:39 +0100

    create post cards, add like ability

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client";
+import { useState } from 'react';
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+export default function Home() {
+    const [likeCount, setLikeCount] = useState(1);
+
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl font-bold">My Status</h1>
+            <p className="text-2xl font-bold">:-D</p>
+            <p className="text-2xl">👍 {likeCount}</p>
+            <button className="bg-blue-500 text-white" onClick={() => setLikeCount(likeCount + 1)}>
+                Like this post
+            </button>
+        </div>
+        //2.26.45
+    );
 }
-
-export default page
